58da718f326c4c97b4182f7c7d05e314e3d30aef
ANTONIO YEE <yee.antonio@gmail.com>
2015-08-19 18:27:39 -0600

    form component

--- app/js/components/Tweet.js
+++ app/js/components/Tweet.js
@@ -4,11 +4,35 @@ import React         from 'react/addons';
 
 var Tweet = React.createClass({
 
-    render() {
+    getInitialState: function() {
+        return {
+            text: ""
+        };
+    },
+
+    handleChange: function(event) {
+        if ( this.state.text.length <= 139) {
+            this.setState({ text: event.target.value });
+        }
+    },
+
+    remainingCharacters: function() {
+        return 140 - this.state.text.length;
+    },
+
+    render: function() {
         return (
-            <h1>tweet</h1>
+            <div className="well clearfix">
+                <textarea className="form-control" onChange={this.handleChange} maxLength={140}></textarea>
+
+                <br/>
+                <span>{ this.remainingCharacters() }</span>
+
+                <button className="btn btn-primary pull-right" disabled={this.state.text.length === 0 }>Tweet</button>
+            </div>
         );
     }
+
 });
 
 export default Tweet;
